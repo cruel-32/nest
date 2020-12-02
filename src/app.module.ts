a457268efdb11a8b736c797b1c7510a9b5ee7bf4
choiseunghee <mz_choeseunghui@woowafriends.com>
2020-12-02 18:48:42 +0900

    - 절대경로 설정

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,10 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 
 import { AppController } from './app.controller';
-import { AppService } from './app.service';
-import { PuduModule } from './pudu/pudu.module';
-import { AuthModule } from './auth/auth.module';
-import { UsersModule } from './users/users.module';
+import { AppService } from '@/app.service';
+import { PuduModule } from '@/pudu/pudu.module';
+import { AuthModule } from '@/auth/auth.module';
+import { UsersModule } from '@/users/users.module';
 
 import { RolesGuard } from './roles.guard';
 
