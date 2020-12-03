fd8a5c2cef891a3aa1dc01af738288dfbb3332dc
choiseunghee <mz_choeseunghui@woowafriends.com>
2020-12-03 21:37:30 +0900

    - sql

---
+++ sql/drop_table.sql
@@ -0,0 +1,11 @@
+drop table pudu_delivery;
+drop table pudu_delivery_log;
+drop table pudu_delivery_detail;
+drop table pudu_robot;
+
+drop table keenon_delivery_detail;
+drop table keenon_delivery;
+drop table keenon_snack;
+drop table keenon_cruise;
+drop table keenon_robot_model;
+drop table keenon_robot;
