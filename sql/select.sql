23bb1c6c66f8ec4c9f1d617463ae95ff13b7abdb
choiseunghee <mz_choeseunghui@woowafriends.com>
2020-12-17 13:19:26 +0900

    - 주차별 서빙 거리

--- sql/select.sql
+++ sql/select.sql
@@ -1,9 +1,15 @@
 select * from pudu_delivery where DATE_FORMAT(create_time, '%Y-%m-%d') = '2020-11-30'
 select * from pudu_delivery where FROM_UNIXTIME(timestamp, '%Y-%m-%d') = '2020-11-30'
 
- SELECT DATE_FORMAT(DATE_SUB(`create_time`, INTERVAL (DAYOFWEEK(`create_time`)-1) DAY), '%Y/%m/%d') as start,
-       DATE_FORMAT(DATE_SUB(`create_time`, INTERVAL (DAYOFWEEK(`create_time`)-7) DAY), '%Y/%m/%d') as end,
-       DATE_FORMAT(`create_time`, '%Y%U') AS `date`,
-       sum(`mileage`)
-  FROM pudu_delivery
- GROUP BY shop_id 
+SELECT DATE_ADD(NOW(), INTERVAL 1 DAY);
+ 
+SELECT
+SHOP_ID, SHOP_NAME
+, SUM(CASE WHEN DATE_FORMAT(create_time, '%Y-%m-%d') BETWEEN '2020-11-01' AND '2020-11-07' THEN mileage END) `11월 첫째주`
+, SUM(CASE WHEN DATE_FORMAT(create_time, '%Y-%m-%d') BETWEEN '2020-11-08' AND '2020-11-14' THEN mileage END) `11월 둘째주`
+, SUM(CASE WHEN DATE_FORMAT(create_time, '%Y-%m-%d') BETWEEN '2020-11-15' AND '2020-11-21' THEN mileage END) `11월 셋째주`
+, SUM(CASE WHEN DATE_FORMAT(create_time, '%Y-%m-%d') BETWEEN '2020-11-22' AND '2020-11-28' THEN mileage END) `11월 넷째주`
+, SUM(CASE WHEN DATE_FORMAT(create_time, '%Y-%m-%d') BETWEEN '2020-11-29' AND '2020-11-30' THEN mileage END) `11월 다섯째주`
+FROM pudu_delivery 
+WHERE DATE_FORMAT(create_time, '%Y-%m-%d') BETWEEN '2020-11-01' AND '2020-11-30' 
+GROUP BY SHOP_ID;
